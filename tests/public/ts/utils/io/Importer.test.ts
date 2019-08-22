633187705f69fc5e8e9f463783ef6a5f9669ebf9
LeonMontealegre <leonm99@gmail.com>
2019-08-21 20:36:45 -0400

    Added initial importer test

---
+++ tests/public/ts/utils/io/Importer.test.ts
@@ -0,0 +1,22 @@
+import "jest";
+
+import {Importer} from "../../../../../site/public/ts/utils/io/Importer";
+
+import {CircuitDesigner} from "../../../../../site/public/ts/models/CircuitDesigner";
+
+describe("Importer", () => {
+    // async function LoadFileContents(path: string): Promise<string> {
+    //     return new Promise<string>((resolve, reject) => {
+    //
+    //     });
+    // }
+
+    test("Basic (v1.1) Circuit Load", (done) => {
+        const designer = new CircuitDesigner(0);
+
+        Importer.LoadCircuit(designer, "");
+
+        expect(designer.getObjects().length).toBe(2);
+        expect(designer.getWires().length).toBe(1);
+    });
+});
